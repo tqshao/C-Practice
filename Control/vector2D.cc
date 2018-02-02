ed41f72067cf2a1d5caa270439d7f12a54d9efcf
tqshao <tqshao@umich.edu>
2018-02-02 09:07:44 -0500

    Complete vector class

--- Control/vector2D.cc
+++ Control/vector2D.cc
@@ -1,14 +1,20 @@
 #include "vector2D.h"
 #include <iostream>
 #include <cmath>
+#include <cassert>
 
 const double pi = 3.1415926;
 
 int main(){
 	vector2D a(3,4);
+	vector2D b(3,4);
+	vector2D c = 2 * a + b;
 	vector2D::createUnitVector(90*pi/180.0);
-	a.debugString();
-	std::cout << a.angle() * 180 / pi << std::endl;
+	double res = a.crossProd(b);
+	// a.normalize();
+	// std::cout <<  "The test result is " << res << std::endl;
+	c.debugString();
+	std::cout << (a == b) << std::endl;
 	return 0;
 }
 
@@ -19,4 +25,97 @@ vector2D vector2D::createUnitVector(const double angle){
 void vector2D::debugString() const{
 	std::cout << "vec2d ( x = " << x_ << ", y = " << y_ << " )" << std::endl;
 }
+
+void vector2D::normalize(){
+	const double l = length();
+	if (l > kMathEpsilon){
+		x_ /= l;
+		y_ /= l;
+	} 
+}
+
+double vector2D::distanceTo(const vector2D& other) const{
+	const double dx = x_ - other.x();
+	const double dy = y_ - other.y();
+	return std::hypot(x_ - other.x(), y_ - other.y());
+}
+
+double vector2D::distanceSquareTo(const vector2D& other) const{
+	const double dx = x_ - other.x();
+	const double dy = y_ - other.y();
+	return dx * dx + dy * dy;
+}
+
+double vector2D::innerProd(const vector2D& other) const{
+	return x_ * other.x() + y_ * other.y();
+}
+
+double vector2D::crossProd(const vector2D& other) const{
+	return x_ * other.y() - y_ * other.x();
+}
+
+vector2D vector2D::rotate(const double angle) const{
+	return vector2D(x_ * cos(angle) - y_ * sin(angle),
+					x_ * sin(angle) + y_ * cos(angle));
+}
+
+vector2D vector2D::operator+(const vector2D &other) const{
+	return vector2D(x_ + other.x(), y_ + other.y());
+}
+
+vector2D vector2D::operator-(const vector2D &other) const{
+	return vector2D(x_ - other.x(), y_ - other.y());
+}
+
+vector2D vector2D::operator*(const double ratio) const{
+	return vector2D(x_ * ratio, y_ * ratio);
+}
+
+vector2D vector2D::operator/(const double ratio) const{
+	assert(ratio > kMathEpsilon);
+	return vector2D(x_ / ratio, y_ / ratio);
+}
+
+vector2D& vector2D::operator+=(const vector2D &other){
+	x_ += other.x();
+	y_ += other.y();
+	return *this;
+}
+
+vector2D& vector2D::operator-=(const vector2D &other){
+	x_ -= other.x();
+	y_ -= other.y();
+	return *this;
+}
+
+vector2D& vector2D::operator*=(const double ratio){
+	x_ *= ratio;
+	y_ *= ratio;
+	return *this;
+}
+
+vector2D& vector2D::operator/=(const double ratio){
+	assert(ratio > kMathEpsilon);
+	x_ /= ratio;
+	y_ /= ratio;
+	return *this;
+}
+
+bool vector2D::operator==(const vector2D& other) const{
+	return (std::abs(x_ - other.x()) < kMathEpsilon && 
+			std::abs(y_ - other.y()) < kMathEpsilon);
+}
+
+vector2D operator*(const double ratio, const vector2D& vec){
+	return vector2D(ratio * vec.x(), ratio * vec.y());
+}
+
+
+
+
+
+
+
+
+
 
--- Control/vector2D.h
+++ Control/vector2D.h
@@ -4,21 +4,88 @@ constexpr double kMathEpsilon = 1e-10;
 
 class vector2D{
 public:
+	// Constructor which takes x and y coordinates
 	vector2D(const double x, const double y) : x_(x), y_(y){}
+	
+	// Constructor returning the zero vector
 	vector2D() : vector2D(0,0){}
+
+	// Creates a unit-vector with a given angle to the positive x semi-axis
 	static vector2D createUnitVector(const double angle);
+	
+	// Returns the x coordinate
 	double x() const { return x_; }
+
+	// Returns the y coordinate
 	double y() const { return y_; }
+
+	// Sets x coordinate
 	double setX(const double x) { x_ = x; }
+
+	// Sets y coordinate
 	double setY(const double y) { y_ = y; }
+
+	// Returns the length of the vector
 	double length() const { return std::hypot(x_,y_); }
-	double lengthSquare() const { return x_ * x_ + y_ * y_; } 
+
+	// Returns the squared length of the vector
+	double lengthSquare() const { return x_ * x_ + y_ * y_; }
+
+	// Returns the angle between the vector and the positive x semi-axis 
 	double angle() const { return std::atan2(y_, x_); }
+
+	// Normalize the vector 
+	void normalize();
+
+	// Output the vector information
 	void debugString() const;
+
+	// Returns the distance to a given vector
+	double distanceTo(const vector2D& other) const;
+
+	// Returns the squared distance to a given vector
+	double distanceSquareTo(const vector2D& other) const;
+
+	// Returns the inner product between the two vectors
+	double innerProd(const vector2D& other) const;
+
+	// Returns the cross product between the two vectors
+	double crossProd(const vector2D& other) const;
+
+	// Rotates the vector counter clockwisely by angle
+	vector2D rotate(const double angle) const;	
+
+	// Sum two vectors
+	vector2D operator+(const vector2D &other) const;
+
+	// Suvtracts two vectors
+	vector2D operator-(const vector2D &other) const;
+
+	// Multiplies this vector by a scaler
+	vector2D operator*(const double ratio) const;
+
+	// Divides this vector by a scalar
+	vector2D operator/(const double ratio) const;
+
+	// Sums another vector to this vector
+	vector2D& operator+=(const vector2D &other);
+
+	// Subtracts another vector to this vector
+	vector2D& operator-=(const vector2D &other);
+
+	// Multiplies this vector by a scalar
+	vector2D& operator*=(const double ratio);
+
+	// Divides this vector by a scalar
+	vector2D& operator/=(const double ratio);
+
+	// Compare two vectors
+	bool operator==(const vector2D& other) const;
 
 protected:
 	double x_ = 0;
 	double y_ = 0;
 };
 
-
+// Multiplies the given vector2D by a given scalar
+vector2D operator*(const double ratio, const vector2D& vec);
