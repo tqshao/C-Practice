274d779f2b7cc158069910bdae4b35d8be508504
tqshao <tqshao@umich.edu>
2018-03-19 22:07:41 -0400

    Added the iteration method.

--- src/Fibo.cc
+++ src/Fibo.cc
@@ -2,27 +2,29 @@
 
 using namespace std;
 
-//0,1,1,2,3
-int fibo(int n){
-	if(n<=0) return 0;
-	else if(n == 2) return 1;
-	else{
-		int fibo;
-		int first = 1;
-		int second = 0;
-		for (int i = 0; i < n-2; i++){
-			fibo = first + second;
-			second = first;
-			first = fibo;
-		}
-		return fibo;
+//0,1,1,2,3,5
+int fibo_recursion(int n){
+	return n < 3 ? n - 1 : fibo_recursion(n-1) + fibo_recursion(n-2);
+}
+
+int fibo_iteration(int n){
+	if(n < 3) return n - 1;
+	int first = 0;
+	int second = 1;
+	int res;
+	for(int i = 0; i < n - 2; i++){
+		res = first + second;
+		first = second;
+		second = res;
 	}
+	return res;
 }
 
 int main()
 {
 	int n;
+	cout << "Type an integer: ";
 	cin >> n;
-	cout << "The nth element of fibo is " << fibo(n) << endl;
+	cout << "The nth element of fibo is " << fibo_iteration(n) << endl;
 	return 0;
 }
