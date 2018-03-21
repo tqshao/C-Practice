87bb8b9b8d415edc526bad4b0ae42e7fec758a87
tqshao <tqshao@umich.edu>
2018-03-20 20:11:49 -0400

    Added other sorting methods

--- src/sort.cc
+++ src/sort.cc
@@ -3,28 +3,42 @@
 
 using namespace std;
 
-void quickSort(vector<int> &array,int begin,int end){
-	if (end>begin){
-		int pivot = begin;
-		int last_small = begin;
-		int i = end;
-		while (last_small != i){
-			if(array[i] <= array[pivot]){
-				// exchange array[++last_small] with array[i]
-				int temp = array[i];
-				array[i] = array[++last_small];
-				array[last_small] = temp;
+void bubbleSort(vector<int> &arr){
+	int size = arr.size();
+	for(int i = 0; i < size - 1; i++){
+		for(int j = i + 1; j < size; j++){
+			if(arr[i] > arr[j]){
+				int tmp = arr[j];
+				arr[j] = arr[i];
+				arr[i] = tmp;
 			}
-			else { i--; }
 		}
-		// exchange array[pivot] with array[last_small]
-		int tmp = array[pivot];
-		array[pivot] = array[last_small];	
-		array[last_small] = tmp;
-		quickSort(array,begin,last_small-1);
-		quickSort(array,last_small+1,end);
 	}
+}
 
+void quickSort(vector<int> &arr,int begin,int end){
+	if(end > begin){
+		int pivot = begin;
+		int change = begin;
+		int i = end;
+		while(change < i){
+			if(arr[i] >= arr[pivot]) i--;
+			else{
+				change++;
+				// exchange [change] with [end]
+				int tmp = arr[change];
+				arr[change] = arr[i];
+				arr[i] = tmp;
+			}
+		}
+		// exchange [pivot] with [change]
+		int tmp = arr[pivot];
+		arr[pivot] = arr[change];
+		arr[change] = tmp;
+		quickSort(arr, begin, change - 1);
+		quickSort(arr, change + 1, end);
+	}
+	return;
 }
 
 void bucketSort(vector<int> &array){
@@ -48,10 +62,10 @@ void bucketSort(vector<int> &array){
 }
 
 int main(){
-	vector<int> a = {1,4,3,2,1,1,3,2,3,4,4};
+	vector<int> a = {1,4,3,2,1,5,1,3,2,3,4,4};
 	// quickSort(a,0,3);
-	bucketSort(a);
+	// quickSort(a,0, a.size() - 1);
+	bubbleSort(a);
 	print(a);
 	return 0;
 }
-
